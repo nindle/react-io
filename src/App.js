1af9f9dfdc5409368207376a23ed76153c502fcd
liuchao <1104892323@qq.com>
2022-02-15 22:32:44 +0800

    state使用

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Dome from "./demo";
-import ReactDOM from "react-dom";
-const user = {
-  age: 18,
-};
 
 const App = () => {
-  const addAge = e => {
-    console.log(e);
-    user.age += 1;
-    // this.setState({ value: e.user.age });
-    // console.log(user.age);
-  };
   return (
     <div className="App">
-      <Dome name="Nindle" age={user.age} />
-      <div id="age" onClick={addAge(user.age)}>
-        年龄增加器{user.age}
-      </div>
+      <Dome />
     </div>
   );
 };
