108210fed50c8e0e25deff307490434fe151451f
liuchao <1104892323@qq.com>
2022-02-17 10:01:29 +0800

    ref使用

--- src/demo.jsx
+++ src/demo.jsx
@@ -15,12 +15,15 @@ class Demo extends React.Component {
   static defaultProps = {
     age: 18, //默认值
   }
+  
   render () {
     const { user } = this.state
     return (
-      <div>
+      <div className='content'>
         <h2>{`我叫${this.props.name}今年${user.age}岁了`}</h2>
         <h3 onClick={this.addAge}>年龄增加器</h3>
+        {/* ref回调函数式 */}
+        <input ref={c => this.inputValue = c} onBlur={this.handleBlur} type="number" placeholder="失去焦点获取数据" /> 
         <img className="img" src={this.props.logo} alt="" />
       </div>
     )
@@ -32,6 +35,10 @@ class Demo extends React.Component {
         age: state.user.age += 1
       }
     }))
+  }
+
+  handleBlur = () => {
+    alert(this.inputValue.value)
   }
 }
 
