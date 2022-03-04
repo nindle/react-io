c4a63dc946d2a26c178cdb346b3efbccb787c5af
liuchao <1104892323@qq.com>
2022-03-04 16:21:17 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Routes, Route, Link, Navigate } from 'react-router-dom';
 // import logo from "./assets/images/logo.svg";
-import "./assets/css/App.css";
+import './assets/css/App.css';
 // import Dome from "./views/demo";
-import Todolist from "./views/todolist";
-import WeiboHotSearch from "./views/WeiboHotSearch";
+import Todolist from './views/todolist';
+import WeiboHotSearch from './views/WeiboHotSearch';
 
 const App = () => {
   return (
@@ -13,13 +13,16 @@ const App = () => {
         <Routes>
           <Route path="/todolist" element={<Todolist />} />
           <Route path="/weiboHotSearch" element={<WeiboHotSearch />} />
-          <Route path="/" element={<Navigate to="/todolist"  />} />
-
+          <Route path="/" element={<Navigate to="/todolist" />} />
         </Routes>
       </div>
       <div>
-        <Link className="list-group-item" to="/todolist">Todolist</Link>
-        <Link className="list-group-item" to="/weiboHotSearch">WeiboHotSearch</Link>
+        <Link className="list-group-item" to="/todolist">
+          Todolist{' '}
+        </Link>
+        <Link className="list-group-item" to="/weiboHotSearch">
+          WeiboHotSearch{' '}
+        </Link>
       </div>
     </div>
   );
