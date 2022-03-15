9b552101df61e2669558096fc14dd701c5e9926e
liuchao <1104892323@qq.com>
2022-03-15 23:15:25 +0800

    youhua

--- src/redux/count_redux.js
+++ src/redux/count_redux.js
@@ -1,6 +1,7 @@
 const defaultCount = 0;
 export default function count(oldValue = defaultCount, action) {
   const { type, data } = action;
+  // 判断type类型
   switch (type) {
     case "add":
       return oldValue + data;
