cec51f60739d5bd1d5642057bee29ae04b2d0708
liuchao <1104892323@qq.com>
2022-03-21 22:47:22 +0800

    hooks练习useEffect

--- src/views/fun/index.jsx
+++ src/views/fun/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 const Fun = () => {
-  const [name, setName] = React.useState('nindle')
-  const onSetName = () => {
-    setName('Ninlde')
+  const [count, setCount] = React.useState(0)
+
+  const onSetCount = () => {
+    setCount(count => count += 1)
   }
+  //模仿生命周期
+  React.useEffect(() => {
+    console.log("初始化调用");
+    return () => {
+      console.log('卸载前调用');
+    }
+    // 写[]表示调用一次，不写每次更新都会调用，[]里面写值表示监听值的变化
+  }, [])
+
   return (
     <div>
-      <h2>{name}</h2>
-      <div onClick={onSetName}>点我改名</div>
+      <h2>{count}</h2>
+      <div onClick={onSetCount}>点我计算</div>
     </div>
   )
 }
