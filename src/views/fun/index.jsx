62bf9d31ff4cae23d7873a22f67eada30bbddbd8
liuchao <1104892323@qq.com>
2022-03-21 22:52:18 +0800

    hooks练习useEffect

--- src/views/fun/index.jsx
+++ src/views/fun/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
+
 const Fun = () => {
   const [count, setCount] = React.useState(0)
 
   const onSetCount = () => {
     setCount(count => count += 1)
   }
+
   //模仿生命周期
   React.useEffect(() => {
     console.log("初始化调用");
@@ -12,12 +15,18 @@ const Fun = () => {
       console.log('卸载前调用');
     }
     // 写[]表示调用一次，不写每次更新都会调用，[]里面写值表示监听值的变化
-  }, [])
+  }, [count])
+
+  // 卸载组件
+  const onUninstall = () => {
+    ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+  }
 
   return (
     <div>
       <h2>{count}</h2>
       <div onClick={onSetCount}>点我计算</div>
+      <div onClick={onUninstall}>卸载组件</div>
     </div>
   )
 }
