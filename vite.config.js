bb54f99fa31fa8a6f54f1a4809f6e7c4518785d9
liuchao <1104892323@qq.com>
2022-03-22 17:32:11 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `$injectedColor: orange;`
-      }
-    }
-  },
   server: {
+    // 配置代理
     proxy: {
       '/weibo': {
         target: 'https://weibo.com',
