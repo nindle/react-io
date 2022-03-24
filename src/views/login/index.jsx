ecc24cb0a90ad229f44dc128e66e56ab049d30c9
liuchao <1104892323@qq.com>
2022-03-24 21:44:24 +0800

    login页面

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './index.scss'
 const Login = () => {
+
   return (
     <div className="login">
       <div>账号: <input type="text" className="text" placeholder='请输入账号' /></div>
       <div>密码: <input type="password" className="password" placeholder='请输入密码' /></div>
+      <button className="submit">登录</button>
     </div>
   )
 }
