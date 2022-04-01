187addb770b60a257e75e4990079b8ac7a8b9b64
liuchao <1104892323@qq.com>
2022-04-01 14:55:45 +0800

    封装函数组件

--- src/components/todolist/List/index.jsx
+++ src/components/todolist/List/index.jsx
@@ -1,24 +1,44 @@
-import React, { PureComponent } from 'react';
+// import React, { PureComponent } from 'react';
 import Item from '../Item';
 import './index.css';
 
-export default class List extends PureComponent {
-  changeShow = (id, e) => {
+// export default class List extends PureComponent {
+//   changeShow = (id, e) => {
+//     if (e === 'delete') {
+//       this.props.del(id);
+//     } else if (e === 'checked') {
+//       this.props.chenge(id);
+//     }
+//   };
+
+//   render() {
+//     const { todos } = this.props;
+//     return (
+//       <ul className="todo-main">
+//         {todos.map(todo => {
+//           return <Item key={todo.id} {...todo} chenge={this.changeShow} />;
+//         })}
+//       </ul>
+//     );
+//   }
+// }
+
+import React from 'react';
+
+export default function List(props) {
+  const changeShow = (id, e) => {
     if (e === 'delete') {
-      this.props.del(id);
+      props.del(id);
     } else if (e === 'checked') {
-      this.props.chenge(id);
+      props.chenge(id);
     }
   };
 
-  render() {
-    const { todos } = this.props;
-    return (
-      <ul className="todo-main">
-        {todos.map(todo => {
-          return <Item key={todo.id} {...todo} chenge={this.changeShow} />;
-        })}
-      </ul>
-    );
-  }
+  return (
+    <ul className="todo-main">
+      {props.todos.map(todo => {
+        return <Item key={todo.id} {...todo} chenge={changeShow} />;
+      })}
+    </ul>
+  );
 }
