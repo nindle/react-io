655d350e217b8091aece8c14a4e19e726e559fb3
liuchao <1104892323@qq.com>
2022-04-01 15:05:00 +0800

    封装函数组件

--- src/components/todolist/End/index.jsx
+++ src/components/todolist/End/index.jsx
@@ -1,38 +1,76 @@
-import React, { PureComponent } from 'react';
+// import React, { PureComponent } from 'react';
 import './index.scss';
 
-export default class End extends PureComponent {
-  onChecks = () => {
-    const { todos, todonum } = this.props;
+// export default class End extends PureComponent {
+//   onChecks = () => {
+//     const { todos, todonum } = this.props;
+//     if (todonum == todos.length) {
+//       this.props.chenge(false);
+//     } else {
+//       this.props.chenge(true);
+//     }
+//   };
+
+//   deleteDoto = () => {
+//     this.props.delete();
+//   };
+
+//   render() {
+//     const { todos, todonum } = this.props;
+//     return (
+//       <div className="todo-footer">
+//         <label>
+//           <input
+//             type="checkbox"
+//             checked={todonum == todos.length && todos.length !== 0}
+//             onChange={this.onChecks}
+//           />
+//         </label>
+//         <span>
+//           <span>已完成 {todonum}</span> / 全部 {todos.length}
+//         </span>
+//         <button className="btn btn-danger show" onClick={this.deleteDoto}>
+//           清除已完成任务
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+import React from 'react';
+
+const index = props => {
+  const { todos, todonum } = props;
+
+  const onChecks = () => {
     if (todonum == todos.length) {
-      this.props.chenge(false);
+      props.chenge(false);
     } else {
-      this.props.chenge(true);
+      props.chenge(true);
     }
   };
 
-  deleteDoto = () => {
-    this.props.delete();
+  const deleteDoto = () => {
+    props.delete();
   };
 
-  render() {
-    const { todos, todonum } = this.props;
-    return (
-      <div className="todo-footer">
-        <label>
-          <input
-            type="checkbox"
-            checked={todonum == todos.length && todos.length !== 0}
-            onChange={this.onChecks}
-          />
-        </label>
-        <span>
-          <span>已完成 {todonum}</span> / 全部 {todos.length}
-        </span>
-        <button className="btn btn-danger show" onClick={this.deleteDoto}>
-          清除已完成任务
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="todo-footer">
+      <label>
+        <input
+          type="checkbox"
+          checked={todonum == todos.length && todos.length !== 0}
+          onChange={onChecks}
+        />
+      </label>
+      <span>
+        <span>已完成 {todonum}</span> / 全部 {todos.length}
+      </span>
+      <button className="btn btn-danger show" onClick={deleteDoto}>
+        清除已完成任务
+      </button>
+    </div>
+  );
+};
+
+export default index;
--- src/components/todolist/Item/index.jsx
+++ src/components/todolist/Item/index.jsx
@@ -1,32 +1,60 @@
-import React, { PureComponent } from 'react';
+// import React, { PureComponent } from 'react';
 import './index.css';
 
-export default class Item extends PureComponent {
-  onChecks = () => {
-    const { id, chenge } = this.props;
+// export default class Item extends PureComponent {
+//   onChecks = () => {
+//     const { id, chenge } = this.props;
+//     chenge(id, 'checked');
+//   };
+
+//   deleteTodo = () => {
+//     const { id, chenge } = this.props;
+//     chenge(id, 'delete');
+//   };
+
+//   render () {
+//     const { name, done } = this.props;
+//     return (
+//       <li>
+//         <label>
+//           <input type="checkbox" checked={done} onChange={this.onChecks} />
+//           <span> {name}</span>
+//         </label>
+//         <button
+//           className={done ? 'btn btn-danger  show' : 'btn btn-danger'}
+//           onClick={this.deleteTodo}
+//         >
+//           删除
+//         </button>
+//       </li>
+//     );
+//   }
+// }
+
+import React from 'react';
+
+const Item = props => {
+  const { name, done, id, chenge } = props;
+
+  const onChecks = () => {
     chenge(id, 'checked');
   };
 
-  deleteTodo = () => {
-    const { id, chenge } = this.props;
+  const deleteTodo = () => {
     chenge(id, 'delete');
   };
 
-  render () {
-    const { name, done } = this.props;
-    return (
-      <li>
-        <label>
-          <input type="checkbox" checked={done} onChange={this.onChecks} />
-          <span> {name}</span>
-        </label>
-        <button
-          className={done ? 'btn btn-danger  show' : 'btn btn-danger'}
-          onClick={this.deleteTodo}
-        >
-          删除
-        </button>
-      </li>
-    );
-  }
-}
+  return (
+    <li>
+      <label>
+        <input type="checkbox" checked={done} onChange={onChecks} />
+        <span> {name}</span>
+      </label>
+      <button className={done ? 'btn btn-danger  show' : 'btn btn-danger'} onClick={deleteTodo}>
+        删除
+      </button>
+    </li>
+  );
+};
+
+export default Item;
