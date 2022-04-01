7490598eedb96d1e402e697a9a950b7f5dab9c76
liuchao <1104892323@qq.com>
2022-04-01 14:37:00 +0800

    封装函数组件

--- src/components/todolist/Head/index.jsx
+++ src/components/todolist/Head/index.jsx
@@ -1,9 +1,39 @@
-import React, { PureComponent } from 'react';
+// import React, { PureComponent } from 'react';
 import { nanoid } from 'nanoid';
 import './index.css';
 
-export default class Head extends PureComponent {
-  onKeyDown = e => {
+// export default class Head extends PureComponent {
+//   onKeyDown = e => {
+//     if (e.keyCode !== 13) {
+//       return;
+//     } else if (e.target.value.trim() !== '') {
+//       const item = {
+//         id: nanoid(),
+//         name: e.target.value,
+//         done: false,
+//       };
+//       this.props.chenge(item);
+//       e.target.value = '';
+//     }
+//   };
+
+//   render () {
+//     return (
+//       <div className="todo-header">
+//         <input
+//           type="text"
+//           onKeyDown={this.onKeyDown}
+//           placeholder="请输入你的任务名称，按回车键确认"
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+import React from 'react';
+
+const Head = props => {
+  const onKeyDown = e => {
     if (e.keyCode !== 13) {
       return;
     } else if (e.target.value.trim() !== '') {
@@ -12,20 +42,16 @@ export default class Head extends PureComponent {
         name: e.target.value,
         done: false,
       };
-      this.props.chenge(item);
+      props.chenge(item);
       e.target.value = '';
     }
   };
 
-  render () {
-    return (
-      <div className="todo-header">
-        <input
-          type="text"
-          onKeyDown={this.onKeyDown}
-          placeholder="请输入你的任务名称，按回车键确认"
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="todo-header">
+      <input type="text" onKeyDown={onKeyDown} placeholder="请输入你的任务名称，按回车键确认" />
+    </div>
+  );
+};
+
+export default Head;
