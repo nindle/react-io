2cf96708d671e2d6ba7a04fb78fef76da1194c4a
liuchao <1104892323@qq.com>
2022-04-01 15:29:48 +0800

    封装函数组件

--- src/components/WeiboHotSearch/Search/index.jsx
+++ src/components/WeiboHotSearch/Search/index.jsx
@@ -1,10 +1,65 @@
-import React, { PureComponent } from 'react';
+// import React, { PureComponent } from 'react';
 import axios from 'axios';
 import PubSub from 'pubsub-js';
 import './index.scss';
-export default class Search extends PureComponent {
+// export default class Search extends PureComponent {
+//   // 获取数据
+//   search = async type => {
+//     if (type === 'weibo') {
+//       const { data } = await axios.get(`/weibo/ajax/side/hotSearch`);
+//       // 发布订阅消息
+//       PubSub.publish('newsList', data.data.realtime);
+//     } else if (type === 'baidu') {
+//       const { data } = await axios.post(`/baidu/api/BaiduHotSearch?hot=rt&qty=30`);
+//       PubSub.publish('newsList', data.data.result);
+//     } else if (type === 'zhihu') {
+//       const { data } = await axios.post(`/v1/metawords/v2/account/login`, {
+//         email: 'gjhjiahao@163.com',
+//         password: 'MTIzNDU2',
+//       });
+//       PubSub.publish('newsList', data.data.result);
+//     }
+//   };
+
+//   componentDidMount () {
+//     this.search('weibo');
+//   }
+
+//   render () {
+//     return (
+//       <div className="search">
+//         <button
+//           ref={c => (this.buttonref = c)}
+//           onClick={() => {
+//             this.search('weibo');
+//           }}
+//         >
+//           微博热搜
+//         </button>
+//         <button
+//           onClick={() => {
+//             this.search('baidu');
+//           }}
+//         >
+//           百度热搜
+//         </button>
+//         <button
+//           onClick={() => {
+//             this.search('zhihu');
+//           }}
+//         >
+//           知乎热搜
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+import React from 'react';
+
+const Search = () => {
   // 获取数据
-  search = async type => {
+  const search = async type => {
     if (type === 'weibo') {
       const { data } = await axios.get(`/weibo/ajax/side/hotSearch`);
       // 发布订阅消息
@@ -21,36 +76,35 @@ export default class Search extends PureComponent {
     }
   };
 
-  componentDidMount () {
-    this.search('weibo');
-  }
+  React.useEffect(() => {
+    search('weibo');
+  }, []);
 
-  render () {
-    return (
-      <div className="search">
-        <button
-          ref={c => (this.buttonref = c)}
-          onClick={() => {
-            this.search('weibo');
-          }}
-        >
-          微博热搜
-        </button>
-        <button
-          onClick={() => {
-            this.search('baidu');
-          }}
-        >
-          百度热搜
-        </button>
-        <button
-          onClick={() => {
-            this.search('zhihu');
-          }}
-        >
-          知乎热搜
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search">
+      <button
+        onClick={() => {
+          search('weibo');
+        }}
+      >
+        微博热搜
+      </button>
+      <button
+        onClick={() => {
+          search('baidu');
+        }}
+      >
+        百度热搜
+      </button>
+      <button
+        onClick={() => {
+          search('zhihu');
+        }}
+      >
+        知乎热搜
+      </button>
+    </div>
+  );
+};
+
+export default Search;
--- src/views/WeiboHotSearch/index.jsx
+++ src/views/WeiboHotSearch/index.jsx
@@ -1,14 +1,27 @@
-import React, { PureComponent } from 'react';
+// import React, { PureComponent } from 'react';
 import List from '../../components/WeiboHotSearch/List';
 import Search from '../../components/WeiboHotSearch/Search';
 import './index.scss';
-export default class WeiboHotSearch extends PureComponent {
-  render () {
-    return (
-      <div className="WeiboHotSearch">
-        <Search />
-        <List />
-      </div>
-    );
-  }
-}
+// export default class WeiboHotSearch extends PureComponent {
+//   render () {
+//     return (
+//       <div className="WeiboHotSearch">
+//         <Search />
+//         <List />
+//       </div>
+//     );
+//   }
+// }
+
+import React from 'react';
+
+const WeiboHotSearch = () => {
+  return (
+    <div className="WeiboHotSearch">
+      <Search />
+      <List />
+    </div>
+  );
+};
+
+export default WeiboHotSearch;
