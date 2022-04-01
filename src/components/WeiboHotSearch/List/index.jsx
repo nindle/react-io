d771b033f2f73c258e9626cb160ef04d8e340623
liuchao <1104892323@qq.com>
2022-04-01 15:39:19 +0800

    转换函数组件

--- src/components/WeiboHotSearch/List/index.jsx
+++ src/components/WeiboHotSearch/List/index.jsx
@@ -1,31 +1,57 @@
-import React, { PureComponent } from 'react';
+// import React, { PureComponent } from 'react';
 import Item from './component/Item';
 import PubSub from 'pubsub-js';
 import './index.scss';
-export default class List extends PureComponent {
-  state = {
-    newsList: [],
-  };
+// export default class List extends PureComponent {
+//   state = {
+//     newsList: [],
+//   };
 
-  componentDidMount() {
-    // 订阅消息
-    this.token = PubSub.subscribe('newsList', (_, data) => {
-      this.setState({ newsList: data });
+//   componentDidMount() {
+//     // 订阅消息
+//     this.token = PubSub.subscribe('newsList', (_, data) => {
+//       this.setState({ newsList: data });
+//     });
+//   }
+
+//   componentWillUnmount() {
+//     // 取消订阅
+//     PubSub.unsubscribe(this.token);
+//   }
+
+//   render() {
+//     return (
+//       <div className="list">
+//         {this.state.newsList.map((item, index) => {
+//           return <Item data={item} key={index} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+import React, { useState } from 'react';
+
+const List = () => {
+  const [newsList, setNewsList] = useState([]);
+
+  React.useEffect(() => {
+    const token = PubSub.subscribe('newsList', (_, data) => {
+      setNewsList(data);
     });
-  }
 
-  componentWillUnmount() {
-    // 取消订阅
-    PubSub.unsubscribe(this.token);
-  }
+    return () => {
+      PubSub.unsubscribe(token);
+    };
+  }, []);
 
-  render() {
-    return (
-      <div className="list">
-        {this.state.newsList.map((item, index) => {
-          return <Item data={item} key={index} />;
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="list">
+      {newsList.map((item, index) => {
+        return <Item data={item} key={index} />;
+      })}
+    </div>
+  );
+};
+
+export default List;
