6bde45921170d22a37b3fcc620437d938045899c
liuchao <1104892323@qq.com>
2022-04-08 11:15:27 +0800

    添加跳转

--- src/components/WeiboHotSearch/List/component/Item.jsx
+++ src/components/WeiboHotSearch/List/component/Item.jsx
@@ -3,10 +3,10 @@ import React, { PureComponent } from 'react';
 export default class Item extends PureComponent {
   render() {
     return (
-      <div className="item">
+      <a className="item" href={this.props.data.url} target="_blank">
         <div>{this.props.data.title}</div>
         <div>{this.props.data.host}</div>
-      </div>
+      </a>
     );
   }
 }
